9535ae09f9d146c75e60bb86d3f0f530030d067c
Vladislav Kostin <v1adyank0stin@gmail.com>
2020-03-27 00:42:52 +0400

    Homeworks done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,63 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+    let x = [];
+    let D = Math.pow(b, 2) - (4 * a * c);
+
+    if(D > 0){
+        
+        x.push((-b + Math.sqrt(D)) / (2 * a));
+        x.push((-b - Math.sqrt(D)) / (2 * a)); 
+    }
+
+    else if(D === 0){
+        x.push(-b / (2 * a));
+    }
+
+    return x;
+    
+};
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    console.log(marks);
+    let evalWhole = 0;
+    
+    if(marks.length > 5 || marks.length === 5){
+        console.log("Оценок пять или больше")
+        marks = marks.slice(0, 5)
+        for(let i = 0; i < marks.length; i++){
+            evalWhole += parseInt(marks[i]);
+        }
+        return evalWhole / marks.length;
+    }
+    else if(marks.length < 5 && marks.length !== 0){
+        for(let i = 0; i < marks.length; i++){
+        evalWhole += parseInt(marks[i]); 
+        }
+        return evalWhole / marks.length;
+   }
+    else {
+        
+        return evalWhole;
+   }
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
+function askDrink(name, dateOfBirthday){
+    let result = (`Не желаете ли олд-фэшн, ${name}?`);
+    let result2 = (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+   
+    let dateNow = new Date();
+    
+    let age = dateNow.getFullYear() - dateOfBirthday.getFullYear();
+    if(age > 18){
+       alert(result);
+       return result;
+    }
+    else{
+      alert(result2);
+      return result2;
+    }
+    //   let result = age > 18 ? alert(`Не желаете ли олд-фэшн, ${name}?`) : alert(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    
     // return result;
 }
