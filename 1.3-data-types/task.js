e294baa684ca366ed031b8fe0ec3c8aca481dc7f
Vladislav Kostin <v1adyank0stin@gmail.com>
2020-03-28 18:29:09 +0400

    BAG

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,25 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    for (let arg of arguments){
+        if(isNaN(arg) || arg === null || arg === undefined){
+            alert("Вводите числовое положительное значение");
+            return
+        }
+    }
+    let dateNow = new Date();
+    
+    let S = amount - contribution; // итоговая сумма займа
+    let P = percent / 12 / 100 // коэффициент месячного процента 
+    let n = Math.round((date.getTime() - dateNow.getTime()) / 2592000000); // разница дат в миллисекундах, разделённая на количество миллисекунд в 31 сутках.
+    console.log(n);
+    let salary = S*(P+P/(((1+P)**n)-1)); // Ежемесячная оплата 
+    let sumWhole = (salary * n).toFixed(2);
+    console.log(sumWhole);
+    return +sumWhole;
+    
+
+
 }
 
 function getGreeting(name) {
